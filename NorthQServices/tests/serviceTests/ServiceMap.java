8e0c7e25f187d1b98b20443edabc411498e9a072
DarkToucan <danmail@mail.dk>
2017-10-06 10:25:57 +0200

    default test space created

---
+++ NorthQServices/tests/serviceTests/ServiceMap.java
@@ -0,0 +1,19 @@
+package serviceTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ServiceMap {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
